cf095a3ada6cf7dc785e12d59e11ac1776a6646e
Andrey543 <andrey_rybakov_97@mail.ru>
2015-10-05 22:34:01 +0300

    Delete 10.py

--- 10.py
+++
@@ -1,5 +0,0 @@
-a = list(map(int, input().split()))
-for i in range(int(input())):
-   a[a[len(a)-1]],a[a[len(a)-1]+1:]=a[len(a)-1],a[a[len(a)-1]:len(a)-1]
-   print(len(a),a[len(a)-1],len(a)-1)
-   print(a)
