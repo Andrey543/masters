6d410e6af3f22577f41e8226832bfaf8e7dd1109
Andrey543 <andrey_rybakov_97@mail.ru>
2015-10-06 10:23:22 +0300

    Тесты для функции factorial

--- test_factorial.py
+++ test_factorial.py
@@ -8,13 +8,13 @@ class LibTest(unittest.TestCase):
 
 
     def test_factorial(self):
-        #self.assertEqual(lib.factorial(0),1)
-        #self.assertEqual(lib.factorial(2),2)
-        #self.assertEqual(lib.factorial(1),1)
-        #self.assertEqual(lib.factorial(5),120)
-        #self.assertEqual(lib.factorial(-2),1)
+        self.assertEqual(lib.factorial(0),1)
+        self.assertEqual(lib.factorial(2),2)
+        self.assertEqual(lib.factorial(1),1)
+        self.assertEqual(lib.factorial(5),120)
+    def test_factorial_1(self):    
         self.assertEqual(lib.factorial(-1),1)
-       # self.assertEqual(lib.factorial(-10),1)
+        
 
 
 # Запускаем тесты на исполнение
