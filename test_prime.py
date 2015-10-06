5e398f976a2a12f0428ae343d9bd65f3343776c2
Andrey543 <andrey_rybakov_97@mail.ru>
2015-10-06 10:24:15 +0300

    Тесты для функции prime

--- test_prime.py
+++ test_prime.py
@@ -7,17 +7,20 @@ import lib
 class LibTest(unittest.TestCase):
 
 
-    def test_prime(self):
+    def test_prime_1(self):
         self.assertEqual(lib.prime(-100),False) #wrong test
+    def test_prime_2(self):
         self.assertEqual(lib.prime(-1),False)   #wrong test
+    def test_prime_3(self):
         self.assertEqual(lib.prime(-2),False)   #wrong test
-        self.assertEqual(lib.prime(-3),False)   #wrong test
-        self.assertEqual(lib.prime(100),False)
+    def test_prime_4(self):    
         self.assertEqual(lib.prime(1),False)    #wrong test
+    def test_prime(self):
         self.assertEqual(lib.prime(2),True)
         self.assertEqual(lib.prime(5),True)
         self.assertEqual(lib.prime(3),True)
         self.assertEqual(lib.prime(22),False)
+        self.assertEqual(lib.prime(100),False)
 
 
 # Запускаем тесты на исполнение
