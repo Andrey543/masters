9b4f45e054a914c433849656d2e8edcaeaca35fd
Andrey543 <andrey_rybakov_97@mail.ru>
2015-10-06 09:35:22 +0300

    Тесты к функции even

---
+++ test_even.py
@@ -0,0 +1,17 @@
+# Подключаем библиотеку для тестирования
+import unittest
+# Подключаем тестируемую библиотеку
+import lib
+
+# Класс, описывающий набор тестов
+class LibTest(unittest.TestCase):
+
+
+    def test_even(self):
+        self.assertEqual(lib.even(0),True)
+        self.assertEqual(lib.even(2),True)
+        self.assertEqual(lib.even(1),False)
+
+
+# Запускаем тесты на исполнение
+unittest.main(verbosity=2)
